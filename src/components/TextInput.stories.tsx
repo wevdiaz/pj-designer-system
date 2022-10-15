cad1f0dbc407d4d620b0dc6e45ca9b0f04033f94
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-10-15 19:18:39 -0300

    criando icone no input atraves da componentização

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,13 +1,17 @@
 import { Meta, StoryObj} from "@storybook/react";
 import { TextInput, TextInputRootProps, TextInputInputProps } from "./TextInput";
+import { Envelope } from "phosphor-react";
 
 export default {
   title : "Components/TextInput",
   component: TextInput.Root,
   args: {
-    children: (
-      <TextInput.Input placeholder="Digite um texto" />
-    )
+    children: [      
+        <TextInput.icon>
+          <Envelope />          
+        </TextInput.icon>,
+        <TextInput.Input placeholder="Digite um texto" />      
+    ]
   },
   argTypes: {
     children: {
@@ -19,4 +23,13 @@ export default {
 } as Meta<TextInputRootProps>
 
 export const Default: StoryObj<TextInputRootProps> = {};
+
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: (
+      <TextInput.Input placeholder="Digite um texto" /> 
+    )
+  }
+};
 
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { InputHTMLAttributes, ReactNode } from "react";
-
+import { Slot } from "@radix-ui/react-slot";
 
 export interface TextInputRootProps {
   children: ReactNode;
@@ -8,13 +8,27 @@ export interface TextInputRootProps {
 
 function TextInputRoot({ children }: TextInputRootProps) {
   return (
-    <div className="flex items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">  
+    <div className="flex items-center gap-3 py-4 px-3 h-12 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">  
       {children}
     </div>
   );
 }
 
-function TextInputIcon() {}
+TextInputRoot.displayName="TextInput.Root";
+
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon({ children }: TextInputIconProps) {
+  return (
+    <Slot className="w-6 h-6 text-gray-400"> 
+      {children}     
+    </Slot>
+  );
+}
+
+TextInputIcon.displayName="TextInput.Icon";
 
 
 export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -30,7 +44,11 @@ function TextInputInput(props: TextInputInputProps) {
   );
 }
 
+TextInputInput.displayName="TextInput.Input";
+
+
 export const TextInput = {
   Root: TextInputRoot,
-  Input: TextInputInput
+  Input: TextInputInput,
+  icon: TextInputIcon
 }
