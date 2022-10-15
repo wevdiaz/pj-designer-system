f0e1d8f985b7a08894520dedb1e352c8a952b81f
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-10-15 16:43:36 -0300

    Criando o componente Heading replicando no storybook

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -27,5 +27,17 @@ export const CustomComponent: StoryObj<TextProps> = {
   args: {
     asChild: true,
     children: ( <p>Testando Slot</p>)
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
+    }
   }
 };
